ca40f5273bc180f1f46e8b0146dbe9db0d438cb3
Chayanon Wichitrnithed <chayanon@laura.oden.utexas.edu>
2022-11-11 17:23:05 -0600

    Work

--- plot61/plot61.py
+++ plot61/plot61.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import datetime
+from scipy.interpolate import interp1d
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
@@ -112,24 +113,73 @@ def getGauge(filename, offset=12):
     elev = elev[of:]
     # time axis in days
     time = np.arange(len(elev))*dt/86400.
-    return time, elev
+    return time, np.array(elev)
 
 
 class Storm():
-    def __init__(self, root, files, gauges):
-        self.root = root
+    def __init__(self, root:str, files:dict, gauges:GaugeList):
+        self.root = root.rstrip("/")
         self.files = files
         self.gauges = gauges
 
-    def plot(self, station, *args, plot_gauge=True, offset=12, timeframe=[]):
+
+    def plot(self, station, *args, plot_gauge=True, adjustMean=False, shift=0, offset=12, timeframe=[]):
         self.f = plt.figure()
+
+        # Get gauge name
+        stationName = self.gauges.gaugeName(station)
+        plt.title(stationName)
+
+        # If we have gauge data, plot it too
+        # Assume the file is named gaugeN.csv
+        #filename = self.root + '/' + self.gauges[station][0] + ".csv"
+        gaugeFile = self.root + '/gauge_' + str(station) + ".csv"
+        if plot_gauge:
+            try:
+                gtime, gelev = getGauge(gaugeFile, offset=offset)
+            except Exception as e:
+                hasGauge = False
+                print(e)
+            else:
+                hasGauge = True
+                if timeframe:
+                    id1 = (np.abs(time - timeframe[0])).argmin()
+                    id2 = (np.abs(time - timeframe[1])).argmin()
+                else:
+                    id1 = 0
+                    id2 = None
+                plt.plot(gtime[id1:id2:3], gelev[id1:id2:3], '--', markersize=7, label='Gauge data')
+                # Interpolate whichever has fewer data and calculate RMSE
+
+        # If time series specified, only plot those
         if args:
             keys = args
         else:
             keys = self.files.keys()
 
+        # Loop over the avaialble time series and plot each one
         for key in keys:
             time, elev = getStation(self.root + '/' + key, station)
+            label = self.files[key]
+
+            # Calculate RMSE compared to gauge data
+            if hasGauge and adjustMean:
+                # First truncate the gauge data
+                gtime_t = gtime[gtime < np.max(time)]
+                gelev_t = gelev[gtime < np.max(time)]
+
+                interp = interp1d(time, elev)
+                newDatum = interp(gtime_t)
+
+                diff = gelev_t - newDatum
+                # Remove NaNs
+                diff = diff[~np.isnan(diff)]
+                meanErr = np.mean(diff)
+                # adjust the time series
+                elev = elev + meanErr
+                print("Adjustment: %.3f m" % meanErr)
+                label = self.files[key]+ ", adjustment: %.3f m" % meanErr
+
             if timeframe:
                 id1 = (np.abs(time - timeframe[0])).argmin()
                 id2 = (np.abs(time - timeframe[1])).argmin()
@@ -137,32 +187,16 @@ class Storm():
                 id1 = 0
                 id2 = None
 
-            plt.plot(time[id1:id2], elev[id1:id2], label=self.files[key])
 
-        # Check if gauge exists
-        # if station in self.gauges:
-        #     plt.title(self.gauges[station][1])
-        stationName = self.gauges.gaugeName(station)
-        plt.title(stationName)
+            # Specified shift in elevation
+            elev = elev + shift
+            if abs(shift) > 0:
+                label = label + ", adjustment: %.3f m" % shift
 
-        # If we have gauge data, plot it too
-        # Assume the file is named gaugeN.csv
-        #filename = self.root + '/' + self.gauges[station][0] + ".csv"
-        filename = self.root + '/gauge_' + str(station) + ".csv"
-        if plot_gauge:
-            try:
-                time, elev = getGauge(filename, offset=offset)
-            except Exception as e:
-                print(e)
-            else:
-                if timeframe:
-                    id1 = (np.abs(time - timeframe[0])).argmin()
-                    id2 = (np.abs(time - timeframe[1])).argmin()
-                else:
-                    id1 = 0
-                    id2 = None
-                plt.plot(time[id1:id2:3], elev[id1:id2:3], '--', markersize=7, label='Gauge data')
+            plt.plot(time[id1:id2], elev[id1:id2], label=label)
 
+
+        # Plot appearance configuration
         plt.legend(loc='best', framealpha=0.0)
         plt.xlabel('Time (days)', fontsize=15)
         plt.ylabel('Surface elevation (m)', fontsize=15)
